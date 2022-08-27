d2d926c49d646f7774abdfbc355076c738767972
laiyutong <yutong729@gmail.com>
2022-08-27 14:43:45 +0800

    0827-10

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 $(function () {
 
+     //手機版
+     if ($(window).width() < 769) {
+        $(".link,figure").remove();
+    }
+
     //Menu套件
-    $('.trigger').on('click', function () {
+    $('.trigger').on('click', function ()
+    {
         $(this).toggleClass('on');
         $('.menu').fadeToggle(200);
     });
