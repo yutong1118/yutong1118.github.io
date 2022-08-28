d3b409d15f16c3c07bea0dd15a9e1681c2e8371f
laiyutong <yutong729@gmail.com>
2022-08-28 23:29:28 +0800

    0828-18

--- script.js
+++ script.js
@@ -12,5 +12,30 @@ $(function () {
         $('.menu').fadeToggle(200);
     });
 
+    const images = $("img");
+
+    function handleScroll() {
+    $.each(images, function (index, image) {
+        const imageMiddleOffset = image.offsetTop + image.height / 2;
+        const scrollAt = window.scrollY + window.innerHeight;
+        if (scrollAt > imageMiddleOffset) {
+        $(this).attr({ class: "active" });
+        }
+    });
+    }
+    //改善效能
+    function debounce (func, delay=250) {
+    let timer = null
+    return () => {
+        let context = this
+        let args = arguments
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+        func.apply(context, args)
+        }, delay)
+    }
+    }
+
+    $(window).scroll(debounce(handleScroll));
 
 });
