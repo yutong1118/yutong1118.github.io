20ce794b996f1c283a7cea8c4f62cdc8393a00b3
laiyutong <yutong729@gmail.com>
2022-09-08 16:18:47 +0800

    0908-1

--- WorkScript.js
+++ WorkScript.js
@@ -1,6 +1,7 @@
     // 判斷元素是否進入當前視窗函式
     function isElementInViewport(el) {
     var rect = el.getBoundingClientRect();
+    // 取得元素「相對於視窗」的座標
     return (
         rect.top >= 0 &&
         rect.left >= 0 &&
@@ -9,12 +10,9 @@
     );
     }
 
-
     // 為視窗可見的列表添加in-view類
     //測試在頁面加載時/向下滾動時是否可見
     var items = document.querySelectorAll(".timeline li");
-
-    // code for the isElementInViewport function
 
     function callbackFunc() {
     for (var i = 0; i < items.length; i++) {
@@ -23,5 +21,6 @@
         }
     }
     }
+
     window.addEventListener("load", callbackFunc);
     window.addEventListener("scroll", callbackFunc);
